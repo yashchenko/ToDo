0ff3fc4d6f31d90120e606c45c1f29aa00322566
Ivan Yashchenko <ivan.profitx@gmail.com>
2025-04-07 09:53:52 +0300

    Initial project setup

---
+++ ToDo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDo
+//
+//  Created by Ivan on 07.04.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
